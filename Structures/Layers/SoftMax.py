added adam optimizer, fixed softmax derivative calcuation for the general case

Structures/Layers/SoftMax.py
@@ -36,10 +36,28 @@ class SoftMax(Layer):
         self.output = softmax(x, axis=axis)
         return self.output
 
-    def backward_pass(self, grads: Union[np.ndarray, cp.ndarray]):
+    def backward_pass(self, **kwargs):
+        grads = kwargs.get('grads')  # Gradient from the next layer, shape: (num_classes, batch_size)
         xp = cp.get_array_module(grads)
-        n = self.output.shape[0]  # neurons / num of classes are the tensor raws
-        return xp.dot((xp.identity(n=n) - self.output.T) * self.output, grads)
+
+        # Output is of shape (num_classes, batch_size)
+        num_classes = self.output.shape[0]
+        batch_size = self.output.shape[1]
+
+        # Initialize the gradient with respect to the input, same shape as the output
+        d_x = xp.empty_like(self.output)
+
+        for i in range(batch_size):
+            # Get the softmax output for the i-th sample (shape: (num_classes,))
+            softmax_out = self.output[:, i].reshape(-1, 1)  # Shape: (num_classes, 1)
+
+            # Compute the Jacobian matrix for the i-th sample
+            jacobian = xp.diagflat(softmax_out) - xp.dot(softmax_out, softmax_out.T)
+
+            # Compute the gradient for this sample (grads[:, i] is shape (num_classes,))
+            d_x[:, i] = xp.dot(jacobian, grads[:, i])
+
+        return d_x
 
     def update_weights(self, **kwargs):
         pass